a700da18e0ff8006c9426a59e9ad6d6b870ff3ce
Arew <noobterr@yandex.ru>
2017-04-21 15:21:47 +0300

    Math objects container Added list that contains all math solar objects

---
+++ Assets/Scripts/Objects/AbstractObjectsContainer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using SolarSystem.Core;
+using SolarSystem.MathObjects;
+
+namespace SolarSystem.MathObjects
+{
+    using SolarMathObjects = List<AbstractObject>;
+    
+    //stores all math objects
+    public static class AbstractObjectsContainer
+    {
+        //main storage field
+        private static SolarMathObjects container;
+
+        static AbstractObjectsContainer()
+        {
+            container = new List<AbstractObject>();
+
+            //create and add math objects
+            container.Add(MathObjectsFactory.create<Stars.Sun>());
+            container.Add(MathObjectsFactory.create<Planets.Mercury>());
+            container.Add(MathObjectsFactory.create<Planets.Venus>());
+            container.Add(MathObjectsFactory.create<Planets.Earth>());
+            container.Add(MathObjectsFactory.create<Planets.Mars>());
+            container.Add(MathObjectsFactory.create<Planets.Jupiter>());
+            container.Add(MathObjectsFactory.create<Planets.Saturn>());
+            container.Add(MathObjectsFactory.create<Planets.Uranus>());
+            container.Add(MathObjectsFactory.create<Planets.Neptune>());
+            container.Add(MathObjectsFactory.create<DwarfPlanets.Pluto>());
+            container.Add(MathObjectsFactory.create<Moons.Moon>());
+        }
+
+        /// <summary>
+        /// Returns abstract object
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static AbstractObject solarObject(Objects obj)
+        {
+            AbstractObject searchedObject = null;
+
+            searchedObject = container.Find(
+                delegate(AbstractObject o)
+                {
+                    return o.objectType() == obj;
+                });
+
+            return searchedObject;
+        }
+
+        /// <summary>
+        /// Returns all abstract math objects
+        /// </summary>
+        /// <returns></returns>
+        public static SolarMathObjects solarObjects()
+        {
+            return container;
+        }
+    }
+}
