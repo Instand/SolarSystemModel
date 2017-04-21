1d852d98b23b7a8a94dc863fd3edcc42dfe7b50f
Arew <noobterr@yandex.ru>
2017-04-21 14:21:46 +0300

    Objects factory Added math objects factory that create generic math objects

---
+++ Assets/Scripts/Objects/MathObjectsFactory.cs
@@ -0,0 +1,18 @@
+﻿using SolarSystem.Core;
+
+namespace SolarSystem
+{
+    //simple factory
+    public static class MathObjectsFactory
+    {
+        /// <summary>
+        /// Creates solar objects
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T create<T>() where T : AbstractObject, new()
+        {
+            return new T();
+        }
+    }
+}
