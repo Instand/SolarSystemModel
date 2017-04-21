4437ed996468b823fa6d626b46fe7d439f29e16c
Arew <noobterr@yandex.ru>
2017-04-21 11:03:16 +0300

    Added solar math core Main math would be here

---
+++ Assets/Scripts/SolarMathModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SolarSystem.Model
+{
+    //represents base solar system model
+    public class SolarMathModel
+    {
+
+    }
+}
