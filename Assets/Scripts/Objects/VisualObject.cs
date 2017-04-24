a32ed739ca6dd9e84e1f03bd5c49b0a2c8520962
Arew <noobterr@yandex.ru>
2017-04-24 09:08:13 +0300

    Added itnerface for solar3d object Base itnerface for 3d object

---
+++ Assets/Scripts/Objects/VisualObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SolarSystem.Interfaces
+{
+    //base interface for any 3d solar object
+    public interface IVisualSolarObject
+    {
+        /// <summary>
+        /// Sets core object type
+        /// </summary>
+        /// <param name="objectType"></param>
+        void setSolarType(Core.Objects objectType);
+
+        /// <summary>
+        /// Returns solar object typeS
+        /// </summary>
+        /// <returns></returns>
+        Core.Objects objectType();
+
+        /// <summary>
+        /// Sets buddy for another solar object
+        /// </summary>
+        /// <param name="buddy"></param>
+        void setBuddy(IVisualSolarObject buddy);
+
+        /// <summary>
+        /// Returns object's buddy object
+        /// </summary>
+        /// <returns></returns>
+        IVisualSolarObject buddy();
+    }
+}
