c0840600eea6d1e753148ca5e3dcf0592d86dd0e
Arew <noobterr@yandex.ru>
2017-05-02 11:44:30 +0300

    Added raycaster for 3d objects interaction

---
+++ Assets/Scripts/Camera/Raycaster.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SolarSystem.Objects3D;
+
+namespace SolarSystem.Controller
+{
+    //casts raycast in the space
+    public class Raycaster : MonoBehaviour
+    {
+        private Camera solarView = null;
+        private RaycastHit hit;
+        private Ray ray;
+
+        //before first frame
+        private void Start()
+        {
+            solarView = Camera.main;
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            //check mouse press
+            if (Input.GetMouseButtonDown(0))
+            {
+                ray = solarView.ScreenPointToRay(Input.mousePosition);
+
+                //cast a ray
+                if (Physics.Raycast(ray, out hit))
+                {
+                    var object3D = hit.transform.GetComponent<Object3D>();
+
+                    //TODO interact with 3d objects
+                    if (object3D)
+                        Debug.Log(object3D.name);
+                }
+            }
+        }
+    }
+}
